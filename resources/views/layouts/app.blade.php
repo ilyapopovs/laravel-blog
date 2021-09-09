047f6788ee6efe36248e54e1fdc81bec87c0e080
ilyapopovs <ilya.popovs.dev@gmail.com>
2021-09-09 22:05:29 +0300

    add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <title>Postful</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="theme-container">
+<body class="theme-container relative pb-24 sm:pb-16" style="min-height: 100vh;">
 <nav class="p-6 bg-theme-primary">
     <div class="container flex justify-between">
         <ul class="h-full flex items-stretch">
@@ -33,6 +33,35 @@
         </ul>
     </div>
 </nav>
+
 @yield('content')
+
+<footer class="absolute bottom-0 w-full h-24 sm:h-16 bg-theme-primary border-t border-theme-primary">
+    <div class="h-full container flex flex-col sm:flex-row justify-center items-center text-theme-primary">
+        <p class="font-sans font-extralight text-sm">Made with Laravel + Vue.js</p>
+        <div class="hidden sm:block text-theme-primary mx-6">|</div>
+        <div class="flex pt-2 sm:pt-0 font-sans font-normal text-sm">
+            <a target="_blank" class="flex items-center mr-4"
+               href="https://github.com/ilyapopovs/postful">
+                <span class="inline-block sm:hidden mr-2">GitHub</span>
+                <svg class="inline logo" fill="hsl(var(--color-text-primary))" xmlns="http://www.w3.org/2000/svg"
+                     width="24"
+                     height="24" viewBox="0 0 24 24">
+                    <path
+                        d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"></path>
+                </svg>
+            </a>
+            <a target="_blank" class="flex items-center" href="https://www.linkedin.com/in/ilyapopovs/">
+                <span class="inline-block sm:hidden mr-2">LinkedIn</span>
+                <svg class="inline logo" fill="hsl(var(--color-text-primary))" xmlns="http://www.w3.org/2000/svg"
+                     width="24"
+                     height="24" viewBox="0 0 24 24">
+                    <path
+                        d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"></path>
+                </svg>
+            </a>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
